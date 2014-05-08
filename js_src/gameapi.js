21c1f9b9aec7aeb3d4cf55a3ecd1fb3e3891d5cc
Wongoo Lee <iwongu@gmail.com>
2014-05-08 05:05:48 -0700

    change back to use x, y, z instead of array.

--- js_src/gameapi.js
+++ js_src/gameapi.js
@@ -9,44 +9,42 @@ var GameApi = function(gameController) {
   }
 };
 
-GameApi.prototype.setBlock = function(position, block) {
-  if (!this.isIn(position)) { return; }
-  if (this.game.getBlock(position) != 0) { return; }
-  this.game.setBlock(position, block);
+GameApi.prototype.setBlock = function(x, y, z, block) {
+  if (!this.isIn(x, y, z)) { return; }
+  if (this.game.getBlock([x, y, z]) != 0) { return; }
+  this.game.setBlock([x, y, z], block);
 };
 
-GameApi.prototype.getBlock = function(position) {
-  if (!this.isIn(position)) { return 0; }
-  return this.game.getBlock([position]);
+GameApi.prototype.getBlock = function(x, y, z) {
+  if (!this.isIn(x, y, z)) { return 0; }
+  return this.game.getBlock([x, y, z]);
 };
 
-GameApi.prototype.removeBlock = function(position) {
-  if (!this.isIn(position)) { return; }
-  this.game.setBlock([position], 0);
+GameApi.prototype.removeBlock = function(x, y, z) {
+  if (!this.isIn(x, y, z)) { return; }
+  this.game.setBlock([x, y, z], 0);
 };
 
-GameApi.prototype.move = function(position) {
-  if (!this.isIn(position)) { return; }
-  this.gameController.avatar.move(position);
+GameApi.prototype.move = function(x, y, z) {
+  if (!this.isIn(x, y, z)) { return; }
+  this.gameController.avatar.move(x, y, z);
 };
 
-GameApi.prototype.moveTo = function(position) {
-  if (!this.isIn(position)) { return; }
-  this.gameController.avatar.moveTo(position);
+GameApi.prototype.moveTo = function(x, y, z) {
+  if (!this.isIn(x, y, z)) { return; }
+  this.gameController.avatar.moveTo(x, y, z);
 };
 
 GameApi.prototype.getTargetPosition = function() {
-  return this.gameController.blockPosPlace ?
+  var pos = this.gameController.blockPosPlace ?
     this.gameController.blockPosPlace : this.gameController.blockPosErase;
+  return pos != null ? {x: pos[0], y: pos[1], z: pos[2]} : null;
 };
 
-GameApi.prototype.isIn = function(position) {
-  for (var i = 0; i < position.length; i++) {
-    if (Math.abs(position[i]) > this.gameController.gameSize) {
-      return false;
-    }
-  }
-  return true;
+GameApi.prototype.isIn = function(x, y, z) {
+  return Math.abs(x) <= this.gameController.gameSize &&
+      Math.abs(y) <= this.gameController.gameSize &&
+      Math.abs(z) <= this.gameController.gameSize;
 };
 
 module.exports = GameApi;
