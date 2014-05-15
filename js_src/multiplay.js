b0e4711c66a0c6b43f17090098a14f3b888fa659
Wongoo Lee <iwongu@gmail.com>
2014-05-14 21:38:11 -0700

    apply new full screen mode in multiplay.

--- js_src/multiplay.js
+++ js_src/multiplay.js
@@ -1,10 +1,12 @@
 'use strict'
 
 var MultiplayController = require('./multiplaycontroller');
+var Topbar = require('./topbar');
 
 var mcApp = angular.module('mc', []);
 
 mcApp.controller('MultiplayController', MultiplayController);
+mcApp.factory('topbar', Topbar);
 mcApp.config(function($httpProvider) {
   $httpProvider.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded";
 });
